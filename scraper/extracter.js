8c615abc55985ac9609265d62517f9f3f42fdc28
weiguo.ni <neefrankie@gmail.com>
2016-06-02 13:40:11 +0800

    update

--- scraper/extracter.js
+++ scraper/extracter.js
@@ -1,11 +1,22 @@
 const fs= require('fs');
 const path = require('path');
-const url = require('url');
-const async = require('async');
-const request = require('request');
 const cheerio = require('cheerio');
 const helper = require('./helper');
 
+function readFilePromisified(file) {
+  return new Promise(
+    function(resolve, reject) {
+      fs.readFile(file, 'utf8', function(err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          const filename = path.basename(file);
+          resolve({filename: filename, content: data});
+        }
+      });
+    }
+  );
+}
 
 function getSvgText(body) {
 
@@ -38,50 +49,32 @@ function getSvgText(body) {
 
 const indexPage = 'ig.ft.com/sites/numbers/economies/china.html';
 
-const testFile = 'ig.ft.com/autograph/graphics/sources-of-china-s-growth.svg';
+readFilePromisified(indexPage)
+// value is an object {filename: , content: }
+  .then(function(value) {
+    console.log(value.filename);
+    const svgFiles = helper.getFilePaths(value.content);
+    console.log(svgFiles);
+    return svgFiles;
+  })
+  .then(function(value) {
+    return Promise.all(value.map(readFilePromisified));
+  })
+  .then(function(value) {
+    const svgTextObj = {};
 
+    value.forEach(function(item) {
+      const svgText = getSvgText(item.content);
+      svgTextObj[item.filename] = svgText;
+      console.log(svgTextObj);
+    });
 
-function extractText(index, callback) {
-  async.waterfall([
-    function(callback) {
-      fs.readFile(index, 'utf8', function(err, body) {
-        if (err) { 
-          return callback(err); 
-        }
-        
-        const filePaths = helper.getFilePaths(body);
-
-        callback(null, filePaths);
-      });
-    },
-    function(filePaths, callback) {
-      const svgText = {};
-
-      filePaths.forEach(function(filePath) {
-
-        fs.readFile(filePath, 'utf8', function(err, body) {
-          if (err) { return callback(err); }
-          filename = path.basename(filePath);
-
-          svgText[filename] = getSvgText(body);
-
-          callback(null, svgText);
-        });
-      });
-    }
-  ], function(err, result) {
-    if (err) {
-      return callback(err);
-    }
-    callback(null, result);
-  });  
-}
-
-extractText(indexPage, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  const jsonResult = JSON.stringify(result, null, 4);
-  const ws = fs.createWriteStream('svg-text-en.json');
-  ws.write(jsonResult);
-});
+    return JSON.stringify(svgTextObj, null, 4);
+  })
+  .then(function(value) {
+    const ws = fs.createWriteStream('svg-text-en.json');
+    ws.write(value);    
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
