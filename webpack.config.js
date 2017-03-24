b26ecb606ab1cb98f3b3b0dc45aab8626456429a
weiguo.ni <neefrankie@gmail.com>
2017-03-24 15:19:53 +0800

    update for webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
-const BowerWebpackPlugin = require('bower-webpack-plugin');
 
 module.exports = {
 	entry: './client/main.js',
@@ -14,21 +12,27 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
-				include: [
-					path.resolve(__dirname, 'client/js'),
-					path.resolve(__dirname, 'bower_components')
-				],
-				loader: 'babel-loader',
-				options: {
-					presets: ['es2015']
-				}
+				resource: {
+					test: /\.js$/,
+					include: [
+						path.resolve(__dirname, 'client'),
+						path.resolve(__dirname, 'bower_components')
+					],
+				},
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['latest']
+						}
+					}
+				]
 			}
 		]
 	},
 	resolve: {
 		modules: [
-			'bower_components',
+			path.resolve(__dirname, 'bower_components'),
 		],
 		mainFields: ['module'],
 		extensions: ['js']
