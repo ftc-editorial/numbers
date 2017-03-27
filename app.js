cbb06253646edd6dd6c9a24e33571091b7c6935b
weiguo.ni <neefrankie@gmail.com>
2017-03-27 16:05:18 +0800

    index page

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const path = require('path');
 const Koa = require('koa')
 const app = new Koa();
 const serve = require('koa-static');
+const footer = require('@ftchinese/ftc-footer')({theme: 'theme-light'});
 const render = require('./util/render.js');
 
 app.use(serve(path.resolve(process.cwd(), 'public')));
+app.use(serve(path.resolve(process.cwd(), 'bower_components')));
 
 app.use(async function(ctx) {
-  ctx.body = await render('index.html');
+  ctx.body = await render('index.html', {
+    pageTitle: '经济数据一图览',
+    footer
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`App listening on prot ${process.eventNames.PORT || 3000}`);
+  console.log(`App listening on port ${process.eventNames.PORT || 3000}`);
 });
