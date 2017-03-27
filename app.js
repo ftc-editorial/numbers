31b35a2200ed3448c79ed6e5a829608a41a04b3f
weiguo.ni <neefrankie@gmail.com>
2017-03-27 10:45:35 +0800

    add koa-static

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const serveStatic = require('serve-static');
+const path = require('path');
 const Koa = require('koa')
 const app = new Koa();
+const serve = require('koa-static');
 const render = require('./util/render.js');
 
-const serve = serveStatic('.tmp', {
-  index: false
-});
-
-app.use(async function(ctx, next) {
-
-});
+app.use(serve(path.resolve(process.cwd(), 'public')));
 
 app.use(async function(ctx) {
   ctx.body = await render('index.html');
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App listening on prot ${process.eventNames.PORT || 3000}`);
+});
