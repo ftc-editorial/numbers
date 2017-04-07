b3c921760be7b2ff651009a8454b70f8867fc92f
weiguo.ni <neefrankie@gmail.com>
2017-04-07 10:32:49 +0800

    rename

---
+++ server/fetch.js
@@ -0,0 +1,27 @@
+function dataUrl(key, republish) {
+  return `https://bertha.ig.ft.com/${republish ? 'republish' : 'view'}/publish/gss/${key}/data,credits,groups,options`;
+}
+
+const docId = {
+  china: '1mzkZNKncQwrVuNw5GbwMYdY3rT54N8vaGXFEhjnJoJA',
+  us: '',
+  uk: '',
+  japan: ''
+};
+
+if (require.main === module) {
+  const path = require('path');
+  const got = require('got');
+  const writeJsonFile = require('write-json-file');
+  const dataUrl = 'https://bertha.ig.ft.com/republish/publish/gss/1mzkZNKncQwrVuNw5GbwMYdY3rT54N8vaGXFEhjnJoJA/data,credits,groups,options';
+  const dest = path.resolve(__dirname, '../test');
+  got(dataUrl, {
+      json: true
+    })
+    .then(res => {
+      return writeJsonFile(`${dest}/example-bertha.json`, res.body);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
--- server/spreadsheet.js
+++ server/spreadsheet.js
@@ -1,27 +1,84 @@
-function dataUrl(key, republish) {
-  return `https://bertha.ig.ft.com/${republish ? 'republish' : 'view'}/publish/gss/${key}/data,credits,groups,options`;
-}
+/** Example data structure in `test` directory.
+ * @param {Object} spreadsheet - JSON fetched from bertha, which in turn fetches from a Google Spreadsheet
+ * @param {Object[]} spreadsheet.options - Meta data
+ * @param {Object[]} spreadsheet.groups - Index
+ * @param {Object[]} spreadsheet.data - Data to be transfromed and merged into `groups`
+ * @param {object[]} spreadsheet.credits
+ */
+function createDashboard(spreadsheet) {
+// Reduce array `spreadsheet.options` to an object,
+// using each array element's `name` as key.
+  const options = spreadsheet.options.reduce((o, row) => {
+    o[row.name] = {text: row.value, html: row.html || row.value};
+    return o;
+  }, {});
 
-const docId = {
-  china: '1mzkZNKncQwrVuNw5GbwMYdY3rT54N8vaGXFEhjnJoJA',
-  us: '',
-  uk: '',
-  japan: ''
-};
+// Reduce array `spreadsheet.data` to an object using each element's `group` as key.
+// Recategorize each element under this key.
+  const cardsByGroup = spreadsheet.data.reduce((o, row) => {
+    const id = row.group;
+    if (o[id]) {
+      o[id].push(row)
+    } else {
+      o[id] = [row];
+    }
+    return o;
+  }, {});
+
+// Add each `spreadsheet.groups` element a key `cards`.
+// Find `card`'s value in `cardsByGroup` identified by `id`.
+  const groups = spreadsheet.groups.map(group => {
+    const id = group.id;
+    const cards = id && cardsByGroup[id];
+    if (id && cardsByGroup.hasOwnProperty(id)) {
+      group.cards = cardsByGroup[id];
+    } else {
+      group.cards = null;
+    }
+    return group;
+  }).filter(group => {
+    return !!group.cards
+  });
+
+// Creat the data structure
+  let dashboard = {
+    title: options.title,
+    introText: options.introText,
+    edition: options.edition,
+    meta: {
+      title: options.title && options.title.text,
+      description: options.description && options.description.text,
+      wx: {
+        image: options['wx:image'] && options['wx:image'].text
+      },
+      og: {
+        site_name: options['og:site_name'] && options['og:site_name'].text,
+        title: options['og:title'] && options['og:title'].text,
+        description: options['og:description'] && options['og:description'].text,
+        image: options['og:image'] && options['og:image'].text
+      }
+    },
+    groups,
+    credits: spreadsheet.credits,
+  }
+  return dashboard;
+}
 
 if (require.main === module) {
   const path = require('path');
-  const got = require('got');
+  const loadJsonFile = require('load-json-file');
   const writeJsonFile = require('write-json-file');
-  const dataUrl = 'https://bertha.ig.ft.com/republish/publish/gss/1mzkZNKncQwrVuNw5GbwMYdY3rT54N8vaGXFEhjnJoJA/data,credits,groups,options';
-  const dest = path.resolve(__dirname, '../data');
-  got(dataUrl, {
-      json: true
+  const dest = path.resolve(__dirname, '../test');
+  loadJsonFile(`${dest}/example-bertha.json`)
+    .then(json => {
+      return createDashboard(json);
     })
-    .then(res => {
-      return writeJsonFile(`${dest}/china.json`, res.body);
+    .then(dashboard => {
+      return writeJsonFile(`${dest}/example-dashboard.json`, dashboard);
     })
     .catch(err => {
       console.log(err);
     });
 }
+
+module.exports = createDashboard;
