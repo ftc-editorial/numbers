9fcb47438f61ef8ecf2526647bffde10fd365732
weiguo.ni <neefrankie@gmail.com>
2017-04-10 11:13:44 +0800

    ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const interpreter = path.resolve(process.env.HOME, 'n/versions/node/7.8.0/bin/node');
+
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "numbers",
+      script    : `app.js`,
+      interpreter: interpreter,
+      env: {
+        NODE_ENV: "development",
+        PORT: 4001,
+      },
+      env_production : {
+        NODE_ENV: "production",
+        PORT: 4001
+      }
+    }
+  ]
+}
