9cb61f7364f631ef6d3a156389724b372f74a6ef
neefrankie <neefrankie@gmail.com>
2017-04-11 21:56:21 +0800

    get the latest data

---
+++ server/latest.js
@@ -0,0 +1,41 @@
+const debug = require('debug')('nums:latest');
+const got = require('got');
+const errors = require('../util/errors.js');
+const url = process.env.LASTEST_DATA_URL ||  'https://ig.ft.com/autograph/data/latest.json';
+
+class Latest {
+  constructor() {
+    this.cache = null;
+  }
+
+  getData() {
+    if (this.cache) {
+      return this.cache;
+    }
+    return got(url, {
+        json: true
+      })
+      .then(response => {
+        debug('Got the latest data');
+        const data = arrayToMap(response.body);
+        this.cache = data;
+        return data;
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+}
+
+function arrayToMap(data) {
+  const map = data.reduce((o, e) => {
+    if(e.id) {
+      e.date = new Date(e.date);
+      o[e.id] = e;
+    }
+    return o;
+  }, {});
+  return map;
+}
+
+module.exports = new Latest();
