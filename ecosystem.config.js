6f3e8cf599871990ef9472eec146e8d60a1d1282
weiguo.ni <neefrankie@gmail.com>
2017-04-11 16:40:22 +0800

    add log timestampl

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,7 @@ module.exports = {
     {
       name      : "numbers",
       script    : "app.js",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
       interpreter: interpreter,
       env: {
         NODE_ENV: "development",
