0622ad7f0e5be35f7055ff1b770485a83494d50c
weiguo.ni <neefrankie@gmail.com>
2017-04-12 17:35:22 +0800

    translate unit

--- server/models/datasets.js
+++ server/models/datasets.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('nums:latest');
 const got = require('got');
 const url = process.env.LASTEST_DATA_URL ||  'https://ig.ft.com/autograph/data/latest.json';
+const unitMap = require('../../translation/unit.json');
 
 class Datasets {
   constructor() {
@@ -36,13 +37,21 @@ function arrayToMap(data) {
       o[e.id] = {
         date: new Date(e.date),
         value: e.value,
-        unit: e.unit,
+        unit: translate(e.unit),
         id: e.id
       };
     }
     return o;
   }, {});
   return map;
+}
+
+function translate(source) {
+  if (unitMap.hasOwnProperty(source)) {
+    return unitMap[source];
+  }
+
+  return source;
 }
 
 module.exports = Datasets;
