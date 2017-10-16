26fc566d66d19ef9f2556cfc6a1818b6309206cd
weiguo.ni <neefrankie@gmail.com>
2017-10-16 12:12:45 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,8 @@ module.exports = {
     // First application
     {
       name      : "numbers",
-      script    : "app.js",
+      script    : "./app.js",
+      cwd: __dirname,
       interpreter: interpreter,
       env: {
         NODE_ENV: "development",
@@ -22,7 +23,23 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 4001,
         DEBUG: "nums*"
-      }
+      },
+      max_restart: 10,
+      error_file: path.resolve(process.env.HOME, 'logs/numbers-err.log'),
+      out_file: path.resolve(process.env.HOME, 'logs/numbers-out.log')
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      user: "node",
+      host: "nodeserver",
+      ref: "origin/master",
+      repo: "https://github.com/ftc-editorial/numbers.git",
+      path: "/home/node/test",
+      "pre-setup": "echo 'pre step'",
+      "post-setup": "ls -la",
+      "pre-deploy-local": "echo 'Begin to deploy'",
+      "post-deploy": "npm install --production && npm run build && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
 }
